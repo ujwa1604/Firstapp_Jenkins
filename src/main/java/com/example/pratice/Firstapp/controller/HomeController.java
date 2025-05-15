98668fc81802d81d5e02301a36def8855f2b88ba
ujwal <ujwal16042002@gmail.com>
2025-05-15 20:54:34 +0530

    changes added

--- src/main/java/com/example/pratice/Firstapp/controller/HomeController.java
+++ src/main/java/com/example/pratice/Firstapp/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home(){
         return "hello world";
     }
+
+    @GetMapping("/send")
+    public String send(){
+        return "successfully send";
+    }
 }
